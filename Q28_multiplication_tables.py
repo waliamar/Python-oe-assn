e55c709690e57ffbadfd2de417c5ab26d6ae5ff4
waliamar <amarwali0987654321@gmail.com>
2022-01-02 17:21:03 +0530

    edits

--- Q28_multiplication_tables.py
+++
@@ -1,4 +0,0 @@
-num = int(input("Type the number you want tables of : "))
-
-for i in range(10):
-    print("{} X {} = {}".format(num, i+1, (num*int(i+1))))
--- file.py
+++ file.py
@@ -1,3 +1,27 @@
-for i in range(5, 23):
+f = open("allprograms.txt", mode="a")
+
+for i in range(2, 33):
+    if i in (10, 13, 21, 31):
+        continue
+    name = "Q"+str(i)+".py"
+    f = open(name, mode="r")
+    xs = f.readlines()
+    f.close()
+    g = open("allprograms.txt", "a")
+    g.write("\n\nQ {} \n\n".format(i))
+    for v in xs:
+        g.write(v)
+    g.close()
+
+for i in range(1, 23):
+    if i in (8, 17, 20):
+        continue
     name = "A2Q"+str(i)+".py"
-    f = open(name, mode="x")
+    f = open(name, mode="r")
+    xs = f.readlines()
+    f.close()
+    g = open("allprograms.txt", "a")
+    g.write("\n\nA2Q {} \n\n".format(i))
+    for v in xs:
+        g.write(v)
+    g.close()
